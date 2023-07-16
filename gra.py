49a0e895c8fdf197a55fac128117052e5b1ba55e
Marek <mareklewandowski@iphbms.pl>
2023-07-16 20:40:54 +0200

    Kwadracik poruszający sie w kwadracie.

--- gra.py
+++ gra.py
@@ -14,6 +14,7 @@ pygame.display.set_caption("Prosta platformówka")
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
 # Stałe dotyczące postaci
 PLAYER_WIDTH = 50
@@ -21,6 +22,10 @@ PLAYER_HEIGHT = 50
 PLAYER_VELOCITY = 5
 JUMP_VELOCITY = 10
 GRAVITY = 0.5
+
+# Stałe dotyczące kwadratu poruszającego się w prawo i skręcającego o 90 stopni
+SQUARE_SIZE = 10
+SQUARE_SPEED = 2
 
 # Pozycja początkowa postaci
 player_x = 50
@@ -37,6 +42,13 @@ platforms = [
 # Zmienne dotyczące ruchu postaci
 player_velocity_y = 0
 is_jumping = False
+
+# Pozycja początkowa kwadratu poruszającego się w prawo i skręcającego o 90 stopni
+square_x = player_x + PLAYER_WIDTH + SQUARE_SIZE
+square_y = player_y + PLAYER_HEIGHT // 2 - SQUARE_SIZE // 2
+
+# Kierunek ruchu kwadratu
+square_direction = 1  # 1 - ruch w prawo, -1 - ruch w lewo
 
 # Zegar dla kontroli liczby klatek na sekundę
 clock = pygame.time.Clock()
@@ -75,6 +87,19 @@ while running:
                 is_jumping = False
                 break
 
+    # Aktualizacja pozycji kwadratu poruszającego się w prawo i skręcającego o 90 stopni
+    square_x += square_direction * SQUARE_SPEED
+
+    # Sprawdzanie, czy kwadrat znajduje się wewnątrz gracza
+    if square_x >= player_x + PLAYER_WIDTH:
+        square_x = player_x + PLAYER_WIDTH
+        square_y = player_y + PLAYER_HEIGHT // 2 - SQUARE_SIZE // 2
+        square_direction = 0.25
+    elif square_x + SQUARE_SIZE <= player_x:
+        square_x = player_x - SQUARE_SIZE
+        square_y = player_y + PLAYER_HEIGHT // 2 - SQUARE_SIZE // 2
+        square_direction = -0.25
+
     # Rysowanie tła
     window.fill(WHITE)
 
@@ -86,9 +111,12 @@ while running:
         pygame.draw.rect(window, BLACK, platform)
 
     # Rysowanie prostokątów gracza i platform
-    pygame.draw.rect(window, WHITE, (player_rect.x+1,player_rect.y+1,player_rect.width-2, player_rect.height-2))
+    pygame.draw.rect(window, WHITE, (player_rect.x+1, player_rect.y+1, player_rect.width-2, player_rect.height-2))
     for platform in platforms:
         pygame.draw.rect(window, WHITE, (platform.x+1, platform.y+1, platform.width-2, platform.height-2))
+
+    # Rysowanie kwadratu poruszającego się w prawo i skręcającego o 90 stopni
+    pygame.draw.rect(window, RED, (square_x, square_y, SQUARE_SIZE, SQUARE_SIZE))
 
     # Aktualizacja okna gry
     pygame.display.update()
